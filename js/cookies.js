a76d867d624f2ef5f9ea8d42ee166821f17ad0ab
Tkdot <95320188+tk-dot@users.noreply.github.com>
2022-01-09 02:51:07 +0700

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -9,5 +9,5 @@ const cookieBox = document.querySelector(".cookie"),
         alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
       }
     }
-    let checkCookie = document.cookie.indexOf("Cookie'By=Danska");
+    let checkCookie = document.cookie.indexOf("CookieBy=Danska");
     checkCookie != -1 ? cookieBox.classList.add("hide") : cookieBox.classList.remove("hide");
